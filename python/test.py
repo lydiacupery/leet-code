3c1b157b2a0ae1f046c628231547d05bf969bad7
Lydia <lydia.cupery@atomicobject.com>
2020-01-18 16:40:32 -0500

    first python solution

---
+++ python/test.py
@@ -0,0 +1,7 @@
+x = int(input("enter your input: "))
+if x < 0:
+	print('hey, that\'s negative!')
+elif x == 0:
+	print('why did you enter 0?')
+else:
+	print('good work, you entered a positive number')
